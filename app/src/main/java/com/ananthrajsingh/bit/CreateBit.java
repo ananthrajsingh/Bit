440d4f2893ca570fdf2d2f6c50cfee2cf858643e
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-04-04 00:41:35 +0530

    3 activities created.

--- app/src/main/java/com/ananthrajsingh/bit/CreateBit.java
+++ app/src/main/java/com/ananthrajsingh/bit/CreateBit.java
@@ -3,13 +3,9 @@ package com.ananthrajsingh.bit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
-import static com.ananthrajsingh.bit.ChooseBitActivity.GOOD_BIT_ID;
 
 public class CreateBit extends AppCompatActivity {
 
-    private TextView tempTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +14,6 @@ public class CreateBit extends AppCompatActivity {
         Intent intent = getIntent();
         int bitId = intent.getIntExtra(getString(R.string.bit_id_extra), 0);
 
-        tempTextView = (TextView) findViewById(R.id.textView_temporary);
-
-        if (bitId == GOOD_BIT_ID){
-            tempTextView.setText("You selected Good Bit");
-        }
-        else {
-            tempTextView.setText("You selected Bad Bit");
-        }
 
     }
 }
