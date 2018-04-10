7cf0efd49ec0e55aeb7ce3eca2846410bccd0310
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-04-10 13:17:13 +0530

    Duplicate commit

--- app/src/main/java/com/ananthrajsingh/bit/CreateBit.java
+++ app/src/main/java/com/ananthrajsingh/bit/CreateBit.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
