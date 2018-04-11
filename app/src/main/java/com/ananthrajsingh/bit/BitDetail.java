2d884718bed28d35c0fc26e8f1f8119bd030167c
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-04-11 15:33:02 +0530

    Added delete functionality

--- app/src/main/java/com/ananthrajsingh/bit/BitDetail.java
+++ app/src/main/java/com/ananthrajsingh/bit/BitDetail.java
@@ -1,10 +1,16 @@
 package com.ananthrajsingh.bit;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.ananthrajsingh.bit.data.BitContract;
+
+import static com.ananthrajsingh.bit.utilities.DatabaseUtils.buildUriToMainTable;
 
 public class BitDetail extends AppCompatActivity {
 
@@ -23,6 +29,17 @@ public class BitDetail extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Uri uri = buildUriToMainTable();
+        String[] selectionArgs = {idOfHabit.toString()};
+        if (id == R.id.action_delete){
+            getContentResolver().delete(uri, BitContract.MainTableEntry._ID + " = ?", selectionArgs);
+        }
         return true;
     }
 }
