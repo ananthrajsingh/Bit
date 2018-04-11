98bb36694a3279c2de482c1fbdf5fa79385a045a
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-04-11 13:40:01 +0530

    Added and inflated menu item in BitDetail activity.

--- app/src/main/java/com/ananthrajsingh/bit/BitDetail.java
+++ app/src/main/java/com/ananthrajsingh/bit/BitDetail.java
@@ -1,13 +1,28 @@
 package com.ananthrajsingh.bit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class BitDetail extends AppCompatActivity {
+
+    public Long idOfHabit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bit_detail);
+        Intent intent = getIntent();
+        idOfHabit = intent.getLongExtra(getString(R.string.item_id_extra), 0);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail, menu);
+        return true;
     }
 }
