985c5ea47612d53a90d86d70283990e650008f97
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-04-28 23:36:18 +0530

    added notification functionality

--- app/src/main/java/com/ananthrajsingh/bit/utilities/NotificationUtils.java
+++ app/src/main/java/com/ananthrajsingh/bit/utilities/NotificationUtils.java
@@ -1,15 +1,21 @@
 package com.ananthrajsingh.bit.utilities;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.NotificationCompat;
 
 import com.ananthrajsingh.bit.MainActivity;
 import com.ananthrajsingh.bit.R;
+
+import static android.os.Build.VERSION_CODES.N;
 
 /**
  * Created by Ananth on 4/27/2018.
@@ -20,6 +26,7 @@ import com.ananthrajsingh.bit.R;
 public class NotificationUtils {
 
     public static final int BIT_PENDING_INTENT_ID = 4388;
+    public static final int NOTIFICATION_ID = 5454;
 
     /**
      * This function will build the notification and display it. It will be helpful to see
@@ -29,7 +36,25 @@ public class NotificationUtils {
     public static void remindUserToUpdate(Context context){
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
-                .set
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_logo)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_body))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                        context.getString(R.string.notification_body)))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+                .setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            notificationBuilder.setPriority(Notification.PRIORITY_HIGH);
+        }
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 
     /**
